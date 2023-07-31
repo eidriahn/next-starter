1d71b96143fbd1923a595015e44dd12fe8a22b25
Adrian Luca <adrianluca14@gmail.com>
2023-07-31 23:09:13 +0200

    :fire: remove tanstack, add urql

--- components/ExampleQuery/ExampleQuery.tsx
+++ components/ExampleQuery/ExampleQuery.tsx
@@ -1,13 +1,3 @@
-"use client";
-
-import { useQuery } from "@tanstack/react-query";
-import { testRequest } from "../../helpers/requests/testRequest";
-
 export const ExampleQuery = () => {
-  const { data } = useQuery({
-    queryKey: ["example"],
-    queryFn: testRequest,
-  });
-
-  return <>{data?.queryData}</>;
+  return <>Example query</>;
 };
